717bd74d15f8bb3054851d87f9709362d29e20d9
sluchyk <boss.sluchyk@gmail.com>
2024-04-25 13:41:19 +0300

    refactoring (main): change ResponseEntity type in GlobalExceptionHandler

--- clear_solutions_task/src/main/java/com/example/clear_solutions_task/web/GlobalExceptionHandler.java
+++ clear_solutions_task/src/main/java/com/example/clear_solutions_task/web/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.clear_solutions_task.web;
 
+import com.example.clear_solutions_task.dto.ResponseDto;
 import com.example.clear_solutions_task.exception.InvalidUserDateInputException;
 import com.example.clear_solutions_task.exception.UniqueUserException;
 import com.example.clear_solutions_task.exception.UserNotFoundException;
@@ -7,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,50 +18,50 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handlerMethodArgumentNotValidException(MethodArgumentNotValidException exception,
+    public ResponseEntity<ResponseDto<String>> handlerMethodArgumentNotValidException(MethodArgumentNotValidException exception,
                                                                          HttpServletRequest request) {
         String errorMessage = Objects.requireNonNull(exception.getBindingResult().getFieldError()).getDefaultMessage();
         log.error("An validation body error occurred for the request URI : {} , \n message : {}"
                 , request.getRequestURI(), errorMessage);
         return ResponseEntity.badRequest()
-                .body(errorMessage);
+                .body(new ResponseDto<>(exception.getMessage(),request.getRequestURI()));
     }
 
     @ExceptionHandler(InvalidUserDateInputException.class)
-    public ResponseEntity<String> handlerInvalidUserDateInputException(InvalidUserDateInputException exception,
-                                                                       HttpServletRequest request) {
+    public ResponseEntity<ResponseDto<String>> handlerInvalidUserDateInputException(InvalidUserDateInputException exception,
+                                                                            HttpServletRequest request) {
         log.error("An validation body error occurred for the request URI : {} , \n message : {}"
                 , request.getRequestURI(), exception.getMessage());
         return ResponseEntity.badRequest()
-                .body(exception.getMessage());
+                .body(new ResponseDto<>(exception.getMessage(),request.getRequestURI()));
 
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> handlerUserNotFoundException(UserNotFoundException exception,
+    public ResponseEntity<ResponseDto<String>> handlerUserNotFoundException(UserNotFoundException exception,
                                                                HttpServletRequest request) {
-        log.error("An validation body error occurred for the request URI : {} , \n message : {}"
-                , request.getRequestURI(), exception.getMessage());
+        log.error("An validation body error occurred for the request URI : {} , \n message : {}",
+                request.getRequestURI(), exception.getMessage());
         return ResponseEntity.badRequest()
-                .body(exception.getMessage());
+                .body(new ResponseDto<>(exception.getMessage(),request.getRequestURI()));
     }
 
     @ExceptionHandler(UniqueUserException.class)
-    public ResponseEntity<String> handlerUniqueUserException(UniqueUserException exception,
+    public ResponseEntity<ResponseDto<String>> handlerUniqueUserException(UniqueUserException exception,
                                                              HttpServletRequest request) {
-        log.error("An validation body error occurred for the request URI : {} , \n message : {}"
-                , request.getRequestURI(), exception.getMessage());
-        return ResponseEntity.badRequest()
-                .body(exception.getMessage());
+        log.error("An validation body error occurred for the request URI : {} , \n message : {}",
+                request.getRequestURI(), exception.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(new ResponseDto<>(exception.getMessage(),request.getRequestURI()));
     }
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
-    public ResponseEntity<String> handlerSQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException exception,
+    public ResponseEntity<ResponseDto<String>> handlerSQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException exception,
                                                              HttpServletRequest request) {
         String errorMessage = "User with such email has been registered";
         log.error("An validation body error occurred for the request URI : {} , \n message : {}"
                 , request.getRequestURI(), exception.getMessage());
-        return ResponseEntity.badRequest()
-                .body(errorMessage);
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(new ResponseDto<>(errorMessage,request.getRequestURI()));
     }
 
 }
